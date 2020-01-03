f3f0002c20c42b399e1569aeeac2e09fe9cc3f9d
mason <9807707@qq.com>
2020-01-03 11:57:32 +0800

    2

--- _test/src/_test/Test1.java
+++ _test/src/_test/Test1.java
@@ -2,8 +2,11 @@ package _test;
 
 public class Test1 {
 	public static void main(String[] args) {
-		String a = "123";
+		String s = "123";
 		int b = 10;
+		
+		System.out.println(s);
+		System.out.println(b+s);
 	}
 
 }
